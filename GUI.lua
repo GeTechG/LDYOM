2b9bf82f7609086515ef6bf7ece277fbdf1f3c99
MGeymer <sergey2003.25.9@gmail.com>
2019-04-14 09:44:49 +0500

    Alpha 0.2.0

--- GUI.lua
+++ GUI.lua
@@ -9,6 +9,9 @@ u8 = encoding.UTF8
 
 ID_Actors = {0, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68, 69, 70, 71, 72, 73, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288}
 
+ID_Cars = {400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 421, 422, 423, 424, 425, 426, 427, 428, 429, 430, 431, 432, 433, 434, 435, 436, 437, 438, 439, 440, 441, 442, 443, 444, 445, 446, 447, 448, 449, 450, 451, 452, 453, 454, 455, 456, 457, 458, 459, 460, 461, 462, 463, 464, 465, 466, 467, 468, 469, 470, 471, 472, 473, 474, 475, 476, 477, 478, 479, 480, 481, 482, 483, 484, 485, 486, 487, 488, 489, 490, 491, 492, 493, 494, 495, 496, 497, 498, 499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 512, 513, 514, 515, 516, 517, 518, 519, 520, 521, 522, 523, 524, 525, 526, 527, 528, 529, 530, 531, 532, 533, 534, 535, 536, 537, 538, 539, 540, 541, 542, 543, 544, 545, 546, 547, 548, 549, 550, 551, 552, 553, 554, 555, 556, 557, 558, 559, 560, 561, 562, 563, 564, 565, 566, 567, 568, 569, 570, 571, 572, 573, 574, 575, 576, 577, 578, 579, 580, 581, 582, 583, 584, 585, 586, 587, 588, 589, 590, 591, 592, 593, 594, 595, 596, 597, 598, 599, 600, 601, 602, 603, 604, 605, 606, 607, 608, 609, 610, 611}
+
+
 local buf_edit_targets_name = imgui.ImBuffer(64);
 local buf_edit_actors_name = imgui.ImBuffer(64);
 local buf_edit_cars_name = imgui.ImBuffer(64);
@@ -48,7 +51,7 @@ function imgui.OnDrawFrame()
 		imgui.Begin(u8'Главное меню', main_window)
 		imgui.Checkbox(u8'Цели',targets_window)
 		imgui.Checkbox(u8'Актёры',actors_window)
-		--imgui.Checkbox(u8'Машины',cars_window)
+		imgui.Checkbox(u8'Машины',cars_window)
 		imgui.Separator()
 		if imgui.Button(u8'Запустить миссию') then
 			miss_start = true
@@ -93,7 +96,7 @@ function imgui.OnDrawFrame()
 		end
 
 		imgui.SameLine()
-		if imgui.Button(u8'Откр/Закр') then
+		if imgui.Button(u8'Откр/Закр') and #list_targets > 0 then
 			list_targets[lb_cur_targets.v+1]['Enable'].v = not list_targets[lb_cur_targets.v+1]['Enable'].v
 		end
 
@@ -103,59 +106,74 @@ function imgui.OnDrawFrame()
 			list_name_targets = DelCellArr(list_name_targets,lb_cur_targets.v+1)
 		end
 
+
 		imgui.End()
-	end
+		--Редактор цели
+		for i = 1, #list_targets do
+			if #list_targets > 0 then
+				if list_targets[i]['Enable'].v then
+					local resX,resY = getScreenResolution()
+					local sizeX,sizeY = 300, 340
+					local targets_list_arr = {u8'Чекпоинт',u8'Сесть в машину',u8'Убить Актёра'}
+					local targets_marker_color = {u8'Красный',u8'Зелёный',u8'Свело-голубой',u8'Белый',u8'Жёлтый'}
+					imgui.SetNextWindowSize(imgui.ImVec2(sizeX,sizeY), imgui.Cond.FirstUseEver)
+					imgui.SetNextWindowPos(imgui.ImVec2((resX-sizeX)/2 - 250,(resY-sizeY)/2 - 30),imgui.Cond.FirstUseEver)
+					imgui.Begin(list_targets[i]['Name'], list_targets[i]['Enable'])
 
-	--Редактор цели
-	for i = 1, #list_targets do
-		if #list_targets > 0 then
-			if list_targets[i]['Enable'].v then
-				local resX,resY = getScreenResolution()
-				local sizeX,sizeY = 300, 340
-				local targets_list_arr = {u8'Чекпоинт'}--,u8'Сесть в машину'}
-				local targets_marker_color = {u8'Красный',u8'Зелёный',u8'Свело-голубой',u8'Белый',u8'Жёлтый'}
-				imgui.SetNextWindowSize(imgui.ImVec2(sizeX,sizeY), imgui.Cond.FirstUseEver)
-				imgui.SetNextWindowPos(imgui.ImVec2((resX-sizeX)/2 - 250,(resY-sizeY)/2 - 30),imgui.Cond.FirstUseEver)
-				imgui.Begin(list_targets[i]['Name'], list_targets[i]['Enable'])
+					imgui.PushItemWidth(-65)
 
-				imgui.PushItemWidth(-65)
-
-				if imgui.Combo(u8'Тип Цели',list_targets[i]['Type'],targets_list_arr) then
-					if list_targets[i]['Type'].v == 0 then
-						local xx,xy,xz = getCharCoordinates(PLAYER_PED)
+					if imgui.Combo(u8'Тип Цели',list_targets[i]['Type'],targets_list_arr) then
 						list_targets[i]['Target_Data'] = {}
-						list_targets[i]['Target_Data']['Pos'] = imgui.ImFloat3(xx,xy,xz)
-						list_targets[i]['Target_Data']['Radius'] = imgui.ImInt(2)
-						list_targets[i]['Target_Data']['Text'] = imgui.ImBuffer(128)
-						list_targets[i]['Target_Data']['Text_time'] = imgui.ImFloat(2)
-						list_targets[i]['Target_Data']['Color_blip'] = imgui.ImInt(0)
+						if list_targets[i]['Type'].v == 0 then
+							local xx,xy,xz = getCharCoordinates(PLAYER_PED)
+							list_targets[i]['Target_Data']['Pos'] = imgui.ImFloat3(xx,xy,xz)
+							list_targets[i]['Target_Data']['Radius'] = imgui.ImInt(2)
+							list_targets[i]['Target_Data']['Text'] = imgui.ImBuffer(128)
+							list_targets[i]['Target_Data']['Text_time'] = imgui.ImFloat(2)
+							list_targets[i]['Target_Data']['Color_blip'] = imgui.ImInt(0)
+						end
+						if list_targets[i]['Type'].v == 1 then
+							list_targets[i]['Target_Data']['Target_car_id'] = imgui.ImInt(0)
+							list_targets[i]['Target_Data']['Color_blip'] = imgui.ImInt(0)
+						end
+						if list_targets[i]['Type'].v == 2 then
+							list_targets[i]['Target_Data']['Target_actor_id'] = imgui.ImInt(0)
+							list_targets[i]['Target_Data']['Color_blip'] = imgui.ImInt(0)
+						end
+
 					end
 
-				end
+					if list_targets[i]['Type'].v == 0 then
+						imgui.InputFloat3(u8'Позиция',list_targets[i]['Target_Data']['Pos'])
 
-				if list_targets[i]['Type'].v == 0 then
-					imgui.InputFloat3(u8'Позиция',list_targets[i]['Target_Data']['Pos'])
-
-					if imgui.Button(u8'Режим перемещения') then
-						editmode_target = true
-						printString(koder('Двигать: Стрелки ~n~Выйти: F'),999999)
-						lockPlayerControl(true)
-						id_target = i
-						imgui.Process = false
+						if imgui.Button(u8'Режим перемещения') then
+							editmode_target = true
+							printString(koder('Двигать: Стрелки ~n~Выйти: F'),999999)
+							lockPlayerControl(true)
+							id_target = i
+							imgui.Process = false
+						end
+						imgui.PushItemWidth(-130)
+						imgui.DragInt(u8'Радиус чекпоинта',list_targets[i]['Target_Data']['Radius'],0.1,-100,100)
+						imgui.Combo(u8'Цвет маркера',list_targets[i]['Target_Data']['Color_blip'],targets_marker_color)
+						imgui.InputText(u8'Текст цели',list_targets[i]['Target_Data']['Text'])
+						imgui.InputFloat(u8'Время текста (сек)',list_targets[i]['Target_Data']['Text_time'])
 					end
-					imgui.PushItemWidth(-130)
-					imgui.DragInt(u8'Радиус чекпоинта',list_targets[i]['Target_Data']['Radius'],0.1,-100,100)
-					imgui.Combo(u8'Цвет маркера',list_targets[i]['Target_Data']['Color_blip'],targets_marker_color)
-					imgui.InputText(u8'Текст цели',list_targets[i]['Target_Data']['Text'])
-					imgui.InputFloat(u8'Время текста (сек)',list_targets[i]['Target_Data']['Text_time'])
-				end
-				if list_targets[i]['Type'].v == 1 then
-				end
+					if list_targets[i]['Type'].v == 1 then
+						imgui.Combo(u8'Машина',list_targets[i]['Target_Data']['Target_car_id'],list_name_cars)
+						imgui.Combo(u8'Цвет маркера',list_targets[i]['Target_Data']['Color_blip'],targets_marker_color)
+					end
+					if list_targets[i]['Type'].v == 2 then
+						imgui.Combo(u8'Актёры',list_targets[i]['Target_Data']['Target_actor_id'],list_name_actors)
+						imgui.Combo(u8'Цвет маркера',list_targets[i]['Target_Data']['Color_blip'],targets_marker_color)
+					end
 
-				imgui.End()
+					imgui.End()
+				end
 			end
 		end
 	end
+
 
 	-- Окно списка актёров
 	if actors_window.v then
@@ -186,6 +204,7 @@ function imgui.OnDrawFrame()
 		--Кнопки редактирования
 		if imgui.Button(u8'Добавить') then
 			local xx,xy,xz = getCharCoordinates(PLAYER_PED)
+			xz = getGroundZFor3dCoord(xx,xy,xz)
 			local angle = getCharHeading(PLAYER_PED)
 			list_actors[#list_actors+1] = {
 				['Name'] = u8'Актёр #' .. tostring(#list_actors+1),
@@ -205,7 +224,7 @@ function imgui.OnDrawFrame()
 		end
 
 		imgui.SameLine()
-		if imgui.Button(u8'Откр/Закр') then
+		if imgui.Button(u8'Откр/Закр') and #list_actors > 0 then
 			list_actors[lb_cur_actors.v+1]['Enable'].v = not list_actors[lb_cur_actors.v+1]['Enable'].v
 		end
 
@@ -214,48 +233,54 @@ function imgui.OnDrawFrame()
 			list_actors = DelCellArr(list_actors,lb_cur_actors.v+1)
 			list_name_actors = DelCellArr(list_name_actors,lb_cur_actors.v+1)
 		end
+
 		imgui.End()
-	end
+		--Редактор актёра
+		for i = 1, #list_actors do
+			if #list_actors > 0 then
+				if list_actors[i]['Enable'].v then
+					local resX,resY = getScreenResolution()
+					local sizeX,sizeY = 300, 340
+					imgui.SetNextWindowSize(imgui.ImVec2(sizeX,sizeY), imgui.Cond.FirstUseEver)
+					imgui.SetNextWindowPos(imgui.ImVec2((resX-sizeX)/2 - 250,(resY-sizeY)/2 - 30),imgui.Cond.FirstUseEver)
+					imgui.Begin(list_actors[i]['Name'], list_actors[i]['Enable'])
+					imgui.PushItemWidth(-90)
 
-	--Редактор актёра
-	for i = 1, #list_actors do
-		if #list_actors > 0 then
-			if list_actors[i]['Enable'].v then
-				local resX,resY = getScreenResolution()
-				local sizeX,sizeY = 300, 340
-				imgui.SetNextWindowSize(imgui.ImVec2(sizeX,sizeY), imgui.Cond.FirstUseEver)
-				imgui.SetNextWindowPos(imgui.ImVec2((resX-sizeX)/2 - 250,(resY-sizeY)/2 - 30),imgui.Cond.FirstUseEver)
-				imgui.Begin(list_actors[i]['Name'], list_actors[i]['Enable'])
-				imgui.PushItemWidth(-90)
-
-				if imgui.InputFloat3(u8'Позиция',list_actors[i]['Actor_Data']['Pos']) or imgui.SliderFloat(u8'Угол поворота',list_actors[i]['Actor_Data']['Angle'],-360,360) then
-					upd_actor:run(i)
-				end
-				if imgui.InputInt(u8'Номер модели',list_actors[i]['Actor_Data']['ModelId']) then
-					local id_a = 0
-					for v = 1,#ID_Actors do
-						if list_actors[i]['Actor_Data']['ModelId'].v <= ID_Actors[v] then
-							id_a = ID_Actors[v]
-							break
-						end
+					if imgui.InputFloat3(u8'Позиция',list_actors[i]['Actor_Data']['Pos']) or imgui.SliderFloat(u8'Угол поворота',list_actors[i]['Actor_Data']['Angle'],-360,360) then
+						local xx,xy,xz = list_actors[i]['Actor_Data']['Pos'].v[1],list_actors[i]['Actor_Data']['Pos'].v[2],list_actors[i]['Actor_Data']['Pos'].v[3]
+						setCharCoordinates(list_actors[i]['Actor_Data']['Char'],xx,xy,xz)
+						setCharHeading(list_actors[i]['Actor_Data']['Char'], list_actors[i]['Actor_Data']['Angle'].v)
 					end
-					print(id_a)
-					list_actors[i]['Actor_Data']['ModelId'].v = id_a
-					upd_actor:run(i)
+					if imgui.InputInt(u8'Номер модели',list_actors[i]['Actor_Data']['ModelId']) then
+						local id_a = 0
+						for v = 1,#ID_Actors do
+							if list_actors[i]['Actor_Data']['ModelId'].v <= ID_Actors[v] then
+								id_a = ID_Actors[v]
+								break
+							end
+						end
+						print(id_a)
+						list_actors[i]['Actor_Data']['ModelId'].v = id_a
+						upd_actor:run(i)
+					end
+					if imgui.Button(u8'Редактировать вручную') then
+						editmode_actor = true
+						id_actor = i
+						deleteChar(list_actors[i]['Actor_Data']['Char'])
+					end
+
+					imgui.PushItemWidth(-110)
+
+					imgui.Separator()
+					local list_tg_m = {u8'До конца'}
+					for ltg = 1,#list_name_targets do
+						list_tg_m[#list_tg_m+1] = list_name_targets[ltg]
+					end
+					imgui.Combo(u8'Появление на',list_actors[i]['Actor_Data']['StartC'],list_name_targets)
+					imgui.Combo(u8'Исчезание после',list_actors[i]['Actor_Data']['EndC'],list_tg_m)
+
+					imgui.End()
 				end
-				if imgui.Button(u8'Редактировать вручную') then
-					editmode_actor = true
-					id_actor = i
-					deleteChar(list_actors[i]['Actor_Data']['Char'])
-				end
-
-				imgui.PushItemWidth(-110)
-
-				imgui.Separator()
-				imgui.Combo(u8'Появление на',list_actors[i]['Actor_Data']['StartC'],list_name_targets)
-				imgui.Combo(u8'Исчезание после',list_actors[i]['Actor_Data']['EndC'],list_name_targets)
-
-				imgui.End()
 			end
 		end
 	end
@@ -289,6 +314,7 @@ function imgui.OnDrawFrame()
 		--Кнопки редактирования
 		if imgui.Button(u8'Добавить') then
 			local xx,xy,xz = getCharCoordinates(PLAYER_PED)
+			xz = getGroundZFor3dCoord(xx, xy, xz+10.0)
 			list_cars[#list_cars+1] = {
 				['Name'] = u8'Машина #' .. tostring(#list_cars+1),
 				['Type'] = imgui.ImInt(-1),
@@ -296,17 +322,18 @@ function imgui.OnDrawFrame()
 				['Car_Data'] = {
 					['Pos'] = imgui.ImFloat3(xx,xy,xz),
 					['Angle'] = imgui.ImFloat(0),
-					['ModelId'] = imgui.ImInt(0),
+					['ModelId'] = imgui.ImInt(400),
 					['StartC'] = imgui.ImInt(0),
 					['EndC'] = imgui.ImInt(0)
 				}
 
 			}
+			upd_car:run(#list_cars)
 			list_name_cars[#list_cars] = list_cars[#list_cars]['Name']
 		end
 
 		imgui.SameLine()
-		if imgui.Button(u8'Откр/Закр') then
+		if imgui.Button(u8'Откр/Закр') and #list_cars > 0 then
 			list_cars[lb_cur_cars.v+1]['Enable'].v = not list_cars[lb_cur_cars.v+1]['Enable'].v
 		end
 
@@ -316,50 +343,56 @@ function imgui.OnDrawFrame()
 			list_name_cars = DelCellArr(list_name_cars,lb_cur_cars.v+1)
 		end
 		imgui.End()
-	end
+		--Редактор машин
+		for i = 1, #list_cars do
+			if #list_cars > 0 then
+				if list_cars[i]['Enable'].v then
+					local resX,resY = getScreenResolution()
+					local sizeX,sizeY = 300, 340
+					imgui.SetNextWindowSize(imgui.ImVec2(sizeX,sizeY), imgui.Cond.FirstUseEver)
+					imgui.SetNextWindowPos(imgui.ImVec2((resX-sizeX)/2 - 250,(resY-sizeY)/2 - 30),imgui.Cond.FirstUseEver)
+					imgui.Begin(list_cars[i]['Name'], list_cars[i]['Enable'])
+					imgui.PushItemWidth(-90)
 
-	--Редактор машин
-	for i = 1, #list_cars do
-		if #list_cars > 0 then
-			if list_cars[i]['Enable'].v then
-				local resX,resY = getScreenResolution()
-				local sizeX,sizeY = 300, 340
-				imgui.SetNextWindowSize(imgui.ImVec2(sizeX,sizeY), imgui.Cond.FirstUseEver)
-				imgui.SetNextWindowPos(imgui.ImVec2((resX-sizeX)/2 - 250,(resY-sizeY)/2 - 30),imgui.Cond.FirstUseEver)
-				imgui.Begin(list_cars[i]['Name'], list_cars[i]['Enable'])
-				imgui.PushItemWidth(-90)
-
-				if imgui.InputFloat3(u8'Позиция',list_cars[i]['Car_Data']['Pos']) or imgui.SliderFloat(u8'Угол поворота',list_cars[i]['Car_Data']['Angle'],-360,360) then
-					upd_actor:run(i)
-				end
-				if imgui.InputInt(u8'Номер модели',list_cars[i]['Car_Data']['ModelId']) then
-					local id_a = 0
-					for v = 1,#ID_Actors do
-						if list_cars[i]['Car_Data']['ModelId'].v <= ID_Actors[v] then
-							id_a = ID_Actors[v]
-							break
-						end
+					if imgui.InputFloat3(u8'Позиция',list_cars[i]['Car_Data']['Pos']) or imgui.SliderFloat(u8'Угол поворота',list_cars[i]['Car_Data']['Angle'],-360,360) then
+						local xx,xy,xz = list_cars[i]['Car_Data']['Pos'].v[1],list_cars[i]['Car_Data']['Pos'].v[2],list_cars[i]['Car_Data']['Pos'].v[3]
+						setCarCoordinates(list_cars[i]['Car_Data']['Car'],xx,xy,xz)
+						setCarHeading(list_cars[i]['Car_Data']['Car'], list_cars[i]['Car_Data']['Angle'].v)
 					end
-					print(id_a)
-					list_cars[i]['Car_Data']['ModelId'].v = id_a
-					upd_actor:run(i)
+					if imgui.InputInt(u8'Номер модели',list_cars[i]['Car_Data']['ModelId']) then
+						local id_c = 400
+						for v = 1,#ID_Cars do
+							if list_cars[i]['Car_Data']['ModelId'].v <= ID_Cars[v] then
+								id_c = ID_Cars[v]
+								break
+							end
+						end
+						print(id_a)
+						list_cars[i]['Car_Data']['ModelId'].v = id_c
+						upd_car:run(i)
+					end
+					if imgui.Button(u8'Редактировать вручную') then
+						editmode_car = true
+						id_car = i
+						deleteCar(list_cars[i]['Car_Data']['Car'])
+					end
+
+					imgui.PushItemWidth(-110)
+
+					imgui.Separator()
+					local list_tg_m = {u8'До конца'}
+					for ltg = 1,#list_name_targets do
+						list_tg_m[#list_tg_m+1] = list_name_targets[ltg]
+					end
+					imgui.Combo(u8'Появление на',list_cars[i]['Car_Data']['StartC'],list_name_targets)
+					imgui.Combo(u8'Исчезание после',list_cars[i]['Car_Data']['EndC'],list_tg_m)
+
+					imgui.End()
 				end
-				if imgui.Button(u8'Редактировать вручную') then
-					editmode_actor = true
-					id_actor = i
-					deleteChar(list_cars[i]['Car_Data']['Car'])
-				end
-
-				imgui.PushItemWidth(-110)
-
-				imgui.Separator()
-				imgui.Combo(u8'Появление на',list_cars[i]['Car_Data']['StartC'],list_name_targets)
-				imgui.Combo(u8'Исчезание после',list_cars[i]['Car_Data']['EndC'],list_name_targets)
-
-				imgui.End()
 			end
 		end
 	end
+
 end
 
 -- Удаление ячейки
@@ -376,13 +409,20 @@ end
 function update_actor(actorr)
 	local modell = list_actors[actorr]['Actor_Data']['ModelId'].v
 	local xx,xy,xz = list_actors[actorr]['Actor_Data']['Pos'].v[1], list_actors[actorr]['Actor_Data']['Pos'].v[2], list_actors[actorr]['Actor_Data']['Pos'].v[3]
-	xz = getGroundZFor3dCoord(xx,xy,xz)
 	local angle = list_actors[actorr]['Actor_Data']['Angle'].v
 
 	requestModel(modell)
 	while not hasModelLoaded(modell) do
 		wait(1)
 	end
+	local last_id = 288
+	for v = 1,#ID_Actors do
+		if modell == ID_Actors[v] then
+			last_id = ID_Cars[v-1]
+			break
+		end
+	end
+	markModelAsNoLongerNeeded(last_id)
 
 	deleteChar(list_actors[actorr]['Actor_Data']['Char'])
 	list_actors[actorr]['Actor_Data']['Char'] = createChar(4,modell,xx,xy,xz)
@@ -390,8 +430,41 @@ function update_actor(actorr)
 	setCharCollision(list_actors[actorr]['Actor_Data']['Char'],false)
 end
 
+function update_car(carr)
+	local modell = list_cars[carr]['Car_Data']['ModelId'].v
+	local xx,xy,xz = list_cars[carr]['Car_Data']['Pos'].v[1], list_cars[carr]['Car_Data']['Pos'].v[2], list_cars[carr]['Car_Data']['Pos'].v[3]
+	local angle = list_cars[carr]['Car_Data']['Angle'].v
+
+	requestModel(modell)
+	while not hasModelLoaded(modell) do
+		wait(1)
+	end
+	local last_id = 611
+	for v = 1,#ID_Cars do
+		if modell == ID_Cars[v] then
+			last_id = ID_Cars[v-1]
+			break
+		end
+	end
+	markModelAsNoLongerNeeded(last_id)
+
+	deleteCar(list_cars[carr]['Car_Data']['Car'])
+	list_cars[carr]['Car_Data']['Car'] = createCar(modell, xx, xy, xz)
+	setCarHeading(list_cars[carr]['Car_Data']['Car'], angle)
+	setCarCollision(list_cars[carr]['Car_Data']['Car'],false)
+	setCarStatus(list_cars[carr]['Car_Data']['Car'], 2)
+	setCarCanBeDamaged(list_cars[carr]['Car_Data']['Car'], false)
+	changeCarColour(list_cars[carr]['Car_Data']['Car'], 1, 1)
+end
+
+
+
+
+
+
 function main()
 	upd_actor = lua_thread.create_suspended(update_actor)
+	upd_car = lua_thread.create_suspended(update_car)
 	while true do
 		wait(0)
 		removeSphere(r)
@@ -540,7 +613,10 @@ function main()
 			for i = 1,#list_actors do
 				deleteChar(list_actors[i]['Actor_Data']['Char'])
 			end
-			mp.start_mission(list_targets,list_actors)
+			for c = 1,#list_cars do
+				deleteCar(list_cars[c]['Car_Data']['Car'])
+			end
+			mp.start_mission(list_targets,list_actors,list_cars)
 			miss_start = false
 		end
 
--- mission_player.lua
+++ mission_player.lua
@@ -6,13 +6,14 @@ encoding = require 'encoding'
 encoding.default = 'CP1251'
 u8 = encoding.UTF8
 mp.actors = {}
+mp.cars = {}
 
 
-function mp.start_mission(listt,lista)
+function mp.start_mission(listt,lista,listc)
 	misflag = mp.flagmis()
 	mp.miss = mp.start()
 	imgui.Process = false
-	mp.main_mission(listt,lista)
+	mp.main_mission(listt,lista,listc)
 end
 
 function mp.flagmis()
@@ -60,7 +61,7 @@ function mp.fall()
   mp.miss = 0
 end
 
-function mp.main_mission(list,list_a)
+function mp.main_mission(list,list_a,list_c)
 	for i = 1,#list do
 		for a = 1,#list_a do
 			if list_a[a]['Actor_Data']['StartC'].v + 1 == i then
@@ -73,8 +74,23 @@ function mp.main_mission(list,list_a)
 				mp.actors[a] = createChar(4, md, xx, xy, xz)
 				setCharHeading(mp.actors[a], list_a[a]['Actor_Data']['Angle'].v)
 			end
-			if list_a[a]['Actor_Data']['EndC'].v + 2 == i then
+			if list_a[a]['Actor_Data']['EndC'].v ~= 0 and list_a[a]['Actor_Data']['EndC'].v + 1 == i then
 				deleteChar(mp.actors[a])
+			end
+		end
+		for c = 1,#list_c do
+			if list_c[c]['Car_Data']['StartC'].v + 1 == i then
+				local md = list_c[c]['Car_Data']['ModelId'].v
+				local xx,xy,xz = list_c[c]['Car_Data']['Pos'].v[1], list_c[c]['Car_Data']['Pos'].v[2], list_c[c]['Car_Data']['Pos'].v[3]
+				requestModel(md)
+				while not isModelAvailable(md) do
+					wait(1)
+				end
+				mp.cars[c] = createCar(md, xx, xy, xz)
+				setCarHeading(mp.cars[c], list_c[c]['Car_Data']['Angle'].v)
+			end
+			if list_c[c]['Car_Data']['EndC'].v ~= 0 and list_c[c]['Car_Data']['EndC'].v + 1 == i then
+				deleteCar(mp.cars[c])
 			end
 		end
 		if list[i]['Type'].v == 0 then
@@ -82,24 +98,53 @@ function mp.main_mission(list,list_a)
 			local xx,xy,xz = list[i]['Target_Data']['Pos'].v[1],list[i]['Target_Data']['Pos'].v[2],list[i]['Target_Data']['Pos'].v[3]
 			rad = list[i]['Target_Data']['Radius'].v
 			--sph[i] = addSphere(xx,xy,xz,rad)
-			resultat,ped = getPlayerChar(PLAYER_HANDLE)
 			printString(koder(u8:decode(list[i]['Target_Data']['Text'].v)),list[i]['Target_Data']['Text_time'].v * 1000)
 			local check = addBlipForCoord(xx,xy,xz)
 			changeBlipColour(check,list[i]['Target_Data']['Color_blip'].v)
-			while not locateCharOnFoot3d(ped,xx,xy,xz,rad,rad,rad,true) do
+			while not locateCharOnFoot3d(PLAYER_PED,xx,xy,xz,rad,rad,rad,true) do
 				wait(0)
 			end
 			removeBlip(check)
 			--removeSphere(sph[i])
 		end
+		if list[i]['Type'].v == 1 then
+			local check = addBlipForCar(mp.cars[list[i]['Target_Data']['Target_car_id'].v+1])
+			changeBlipColour(check, 1)
+			printString(koder('Цель: сесть в ~b~машину'), 2000)
+			while not isCharInCar(PLAYER_PED, mp.cars[list[i]['Target_Data']['Target_car_id'].v+1]) do
+				wait(0)
+			end
+			removeBlip(check)
+		end
+		if list[i]['Type'].v == 2 then
+			local check = addBlipForChar(mp.actors[list[i]['Target_Data']['Target_actor_id'].v+1])
+			changeBlipColour(check, 0)
+			printString(koder('Цель: убей ~r~машину'), 2000)
+			while not isCharDead(mp.actors[list[i]['Target_Data']['Target_actor_id'].v+1]) do
+				wait(0)
+			end
+			removeBlip(check)
+		end
+	end
+	mp.pass(1)
+	if isCharInAnyCar(PLAYER_PED) then
+		taskLeaveAnyCar(PLAYER_PED)
+		while isCharInAnyCar(PLAYER_PED) do
+			wait(0)
+		end
 	end
 	for v,h in pairs(mp.actors) do
 		deleteChar(mp.actors[v])
 	end
+	for v,h in pairs(mp.cars) do
+		deleteCar(mp.cars[v])
+	end
 	for j = 1,#list_a do
 		upd_actor:run(j)
 	end
-	mp.pass(1)
+	for j = 1,#list_c do
+		upd_car:run(j)
+	end
 end
 
 return mp
