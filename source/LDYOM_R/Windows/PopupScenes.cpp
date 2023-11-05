e8118ec05ceeaba43dde3de9db69d55ab8ead1a8
GeTechG <sergey2003.25.9@gmail.com>
2023-11-05 17:44:39 +0500

    fix: Addressed issue #25 (crash when clicking 'load scene') and added prevention for loading the current scene
    refactor(PopupScenes.cpp): reformat code for better readability and consistency
    fix(PopupScenes.cpp): fix incorrect include order
    feat(PopupScenes.cpp): add support for selecting and loading scenes in the popup window
    feat(PopupScenes.cpp): add support for creating new scenes in the current project
    feat(PopupScenes.cpp): add support for deleting scenes from the current project

--- source/LDYOM_R/Windows/PopupScenes.cpp
+++ source/LDYOM_R/Windows/PopupScenes.cpp
@@ -1,18 +1,18 @@
 ﻿#include "PopupScenes.h"
 
-#include "imgui.h"
-#include "fmt/core.h"
 #include "fa.h"
+#include "imgui.h"
 #include "ProjectsService.h"
+#include "fmt/core.h"
 #include "Localization/Localization.h"
 
 void Windows::PopupScenes::open() {
 	AbstractWindow::open();
 }
 
-void scenesList(int& selected_scene, const ImVec2 window_size) {
+void scenesList(int &selected_scene, const ImVec2 window_size) {
 	ImGui::BeginChild("##scenesList", ImVec2(window_size.x / 3.0f, window_size.y / 3.0f), true);
-	for (const auto& pair : ProjectsService::getInstance().getCurrentProject().getScenes()) {
+	for (const auto &pair : ProjectsService::getInstance().getCurrentProject().getScenes()) {
 		if (ImGui::Selectable(pair.second->getName(), selected_scene == pair.first)) {
 			selected_scene = pair.first;
 		}
@@ -21,35 +21,47 @@ void scenesList(int& selected_scene, const ImVec2 window_size) {
 }
 
 void Windows::PopupScenes::draw() {
-	static auto& local = Localization::getInstance();
+	static auto &local = Localization::getInstance();
 
 	static int selected_scene = -1;
 
 	const ImVec2 displaySize = ImGui::GetIO().DisplaySize;
 	ImGui::SetNextWindowPos(ImVec2(displaySize.x / 2.f, displaySize.y / 2.f), ImGuiCond_Appearing, ImVec2(.5f, .5f));
-	if (ImGui::BeginPopupModal(fmt::format("{} {}", ICON_FA_LANDMARK, local.get("scenes.title")).c_str(), &this->isShow(), ImGuiWindowFlags_AlwaysAutoResize)) {
-
+	if (ImGui::BeginPopupModal(fmt::format("{} {}", ICON_FA_LANDMARK, local.get("scenes.title")).c_str(),
+	                           &this->isShow(), ImGuiWindowFlags_AlwaysAutoResize)) {
 		scenesList(selected_scene, displaySize);
 		// (1/3 window) / (3 buttons) - 3.0
 		const auto widthButton = ImGui::GetContentRegionAvail().x / 3.0f - 3.0f;
+		auto &currentProject = ProjectsService::getInstance().getCurrentProject();
 		if (ImGui::Button(local.get("scenes.new_scene").c_str(), ImVec2(widthButton, .0f))) {
-			ProjectsService::getInstance().getCurrentProject().newScene();
-			selected_scene = ProjectsService::getInstance().getCurrentProject().getCurrentSceneIndex();
+			currentProject.newScene();
+			selected_scene = currentProject.getCurrentSceneIndex();
 		}
 		ImGui::SameLine();
-		if (ImGui::Button(local.get("scenes.load_scene").c_str(), ImVec2(widthButton, .0f))) {
-			ProjectsService::getInstance().getCurrentProject().changeScene(selected_scene);
-		}
-		ImGui::SameLine();
+		const bool hasScene = !currentProject.getScenes().contains(selected_scene);
+		ImGui::BeginDisabled(selected_scene == -1 || hasScene);
+		{
+			ImGui::BeginDisabled(
+				currentProject.getCurrentSceneIndex() == selected_scene);
+			{
+				if (ImGui::Button(local.get("scenes.load_scene").c_str(), ImVec2(widthButton, .0f))) {
+					currentProject.changeScene(selected_scene);
+					this->close();
+				}
+			}
+			ImGui::EndDisabled();
+			ImGui::SameLine();
 
-		ImGui::BeginDisabled(ProjectsService::getInstance().getCurrentProject().getScenes().size() <= 1);
-		if (ImGui::Button(local.get("scenes.delete_scene").c_str(), ImVec2(widthButton, .0f))) {
-			ImGui::OpenPopup(popupDelete_.getName());
+			ImGui::BeginDisabled(currentProject.getScenes().size() <= 1);
+			if (ImGui::Button(local.get("scenes.delete_scene").c_str(), ImVec2(widthButton, .0f))) {
+				ImGui::OpenPopup(popupDelete_.getName());
+			}
+			ImGui::EndDisabled();
 		}
 		ImGui::EndDisabled();
 
-		this->popupDelete_.draw([] {
-			ProjectsService::getInstance().getCurrentProject().deleteScene(selected_scene);
+		this->popupDelete_.draw([&] {
+			currentProject.deleteScene(selected_scene);
 		});
 
 		ImGui::EndPopup();
