ea35092657e477edbb3b18781732999610e3e43f
GeTechG <sergey2003.25.9@gmail.com>
2023-11-12 04:07:14 +0500

    feat: Introduced auto-translation tool
    chore(.gitignore): add .env file to the global .gitignore
    feat(.gitignore): add .idea/ and tools/translater/ to the .gitignore
    feat(.idea/encodings.xml): add encodings.xml file to configure encoding for Russian.toml file
    feat(.idea/inspectionProfiles/profiles_settings.xml): add profiles_settings.xml file to configure inspection profiles
    feat(.idea/misc.xml): add misc.xml file to configure Black formatter and Python SDK version
    feat(.idea/modules.xml): add modules.xml file to configure project modules
    feat(.idea/translater.iml): add translater.iml file to configure Python module
    feat(.idea/vcs.xml): add vcs.xml file to configure VCS directory mappings
    feat(tools/translater/main.py): add main.py file for translating text using OpenAI API and saving translations to toml files in langs/ directory

---
+++ tools/translater/main.py
@@ -0,0 +1,78 @@
+import json
+from itertools import batched
+from dotenv import load_dotenv
+from openai import OpenAI
+import toml
+import time
+from os import walk
+
+load_dotenv()
+
+client = OpenAI()
+instruction = """"You are an advanced artificial intelligence translator. In your request, you will receive a JSON object with keys and {} text for translation. This text is intended for the interface of a mod for the game GTA SA, focused on creating custom missions. Please consider this context when translating. Your response should include a JSON object with the same keys, but the values should be the translated text. Translation language - {}."""
+
+default_lang_name = 'Russian'
+
+def translate(fields: dict, current_lang: str) -> dict:
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        response_format={"type": "json_object"},
+        messages=[
+            {"role": "system", "content": instruction.format(default_lang_name, current_lang)},
+            {"role": "user", "content": json.dumps(fields)}
+        ]
+    )
+
+    return json.loads(completion.choices[0].message.content)
+
+
+langs = {}
+
+folder_path = 'langs'
+for root, dirs, files in walk(folder_path):
+    for file in files:
+        if file.endswith('.toml'):
+            with open(f'{folder_path}/{file}', 'r', encoding='utf-8') as f:
+                lang = toml.load(f)
+                langs[file[:-5]] = lang
+
+needs_langs = ['English']
+
+
+def translate_langs():
+    if default_lang_name not in langs:
+        raise Exception('Default language not found')
+
+    default_lang = langs[default_lang_name]
+
+    for iter_lang in needs_langs:
+        if iter_lang not in langs:
+            langs[iter_lang] = {}
+
+        if iter_lang != default_lang_name:
+            print(f'Generating {iter_lang}...')
+            i = 0
+            for category in default_lang:
+                if category not in langs[iter_lang]:
+                    langs[iter_lang][category] = {}
+
+                values: dict = default_lang[category]
+                for bath_keys in batched(filter(lambda s: s not in langs[iter_lang][category], values.keys()), 50):
+                    translated = translate({key: values[key] for key in bath_keys}, iter_lang)
+                    print(translated)
+                    langs[iter_lang][category].update(translated)
+                    i += 1
+                    if i == 3:
+                        time.sleep(62)
+                        i = 0
+
+
+try:
+    translate_langs()
+except Exception as e:
+    print('Error:', e)
+
+for lang in langs:
+    if lang != default_lang_name:
+        with open(f'langs/{lang}.toml', 'w', encoding='utf-8') as f:
+            toml.dump(langs[lang], f)
