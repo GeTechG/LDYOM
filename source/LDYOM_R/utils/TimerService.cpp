e2db9607c043d03269ab6b5cc8178a63f5694fff
GeTechG <sergey2003.25.9@gmail.com>
2023-12-11 22:42:31 +0500

    refactor: reorder include statements, fix parameter formatting, use reference in TimerService.cpp

--- source/LDYOM_R/utils/TimerService.cpp
+++ source/LDYOM_R/utils/TimerService.cpp
@@ -1,7 +1,7 @@
 ﻿#include "TimerService.h"
 
-#include <CUserDisplay.h>
 #include <CTheScripts.h>
+#include <CUserDisplay.h>
 
 uintptr_t spaceVarsPtr = NULL;
 uintptr_t timer = NULL;
@@ -10,14 +10,14 @@ bool& TimerService::isTimerOn() {
 	return timerOn_;
 }
 
-void TimerService::addTimer(const char* text, const bool direction, const int value) {
+void TimerService::addTimer(const char *text, const bool direction, const int value) {
 	spaceVarsPtr = reinterpret_cast<uintptr_t>(CTheScripts::ScriptSpace);
 	timer = 12640;
 
 	if (this->isTimerOn())
 		return;
 
-	auto& address = *reinterpret_cast<int*>(spaceVarsPtr + timer);
+	auto &address = *reinterpret_cast<int*>(spaceVarsPtr + timer);
 	address = value;
 
 	CUserDisplay::OnscnTimer.AddClock(timer, const_cast<char*>(text), direction);
