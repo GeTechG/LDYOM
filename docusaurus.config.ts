b2ee75c1a4dcb380dc98145f162b2a7a0c6c9344
GeTechG <sergey2003.25.9@gmail.com>
2024-03-07 22:18:27 +0500

    feat(docs): Add Installation Guide for Beta 0.8+ Version

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -67,7 +67,7 @@ const config: Config = {
       items: [
         {
           type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
+          sidebarId: 'docs',
           position: 'left',
           label: 'Docs',
         },
