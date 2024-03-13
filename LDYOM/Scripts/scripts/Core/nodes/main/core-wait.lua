ad11b0c03e4ec381ff6b627e47c8fc7d483256ff
GeTechG <sergey2003.25.9@gmail.com>
2024-03-13 12:01:19 +0500

    feat: Added a time parameter to the input of the "wait" node.

--- LDYOM/Scripts/scripts/Core/nodes/main/core-wait.lua
+++ LDYOM/Scripts/scripts/Core/nodes/main/core-wait.lua
@@ -6,6 +6,7 @@ local fa = require("libs.fa")
 
 ---@class LDNodeEditorWaitNode : LDNodeEditorNode
 ---@field waitTime number
+---@field version integer
 
 local WAIT_NODE_TYPE = "core.wait"
 
@@ -31,6 +32,12 @@ local waitNode = {
                     kind = NodeEditorPinKind.Input,
                     type = "core.flow",
                 },
+                {
+                    id = getPinId(),
+                    node = newNodeId,
+                    kind = NodeEditorPinKind.Input,
+                    type = "core.number",
+                }
             },
             outputs = {
                 {
@@ -40,7 +47,8 @@ local waitNode = {
                     type = "core.flow",
                 },
             },
-            waitTime = 0
+            waitTime = 0,
+            version = 1
         }
         return newNode
     end,
@@ -51,17 +59,27 @@ local waitNode = {
     draw = function(editor, ctx, node, builder)
         local fontScale = ImGui.GetFontSize() / 16;
 
+        if node.version == nil then
+            node.version = 1
+            node.inputs[#node.inputs+1] = {
+                id = node.id + 3,
+                node = node.id,
+                kind = NodeEditorPinKind.Input,
+                type = "core.number",
+            }
+        end
+
         builder:Begin(NodeEditor.NodeId(node.id));
         LDNodeEditor.defaultHeader(editor, builder, node);
 
         LDNodeEditor.defaultInput(editor, ctx, builder, node.inputs[1], "");
-
-        ImGui.SameLine(0,-1);
-        ImGui.SetNextItemWidth(100 * fontScale);
-        local isEdit, newWaitTime = ImGui.InputFloat(ld.loc.get("general.time"), node.waitTime, 0, 0, "%.3f", 0);
-        if isEdit then
-            node.waitTime = newWaitTime
-        end
+        LDNodeEditor.defaultInput(editor, ctx, builder, node.inputs[2], ld.loc.get("general.time"), function ()
+            ImGui.SetNextItemWidth(100 * fontScale);
+            local isEdit, newWaitTime = ImGui.InputFloat("##editTime", node.waitTime, 0, 0, "%.3f", 0);
+            if isEdit then
+                node.waitTime = newWaitTime
+            end
+        end);
 
         LDNodeEditor.defaultOutput(editor, ctx, builder, node.outputs[1], "");
 
@@ -72,7 +90,8 @@ local waitNode = {
     ---@param node LDNodeEditorWaitNode
     ---@param inputValues any[]
     run = function(editor, context, node, inputValues)
-        _ = coroutine.yield(node.waitTime * 1000)
+        local waitTime = inputValues[2] or node.waitTime
+        _ = coroutine.yield(waitTime * 1000)
         return {1}
     end
 }
