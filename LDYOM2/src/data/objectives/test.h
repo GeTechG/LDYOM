d129d7eb700f9802443bdaaf46a0128db326f4fb
GeTechG <sergey2003.25.9@gmail.com>
2025-05-04 03:20:24 +0500

    feat: add test objective to demonstrate objective creation

---
+++ LDYOM2/src/data/objectives/test.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "../objective.h"
+#include <imgui.h>
+#include <localization.h>
+
+constexpr const char* TEST_OBJECTIVE_TYPE = "core.test";
+
+struct TestObjectiveData {
+	int value = 0;
+	std::string value2 = "default";
+};
+
+void renderTestObjectiveEditor(TestObjectiveData& data) {
+	ImGui::InputInt("Value", &data.value);
+	ImGui::Text("Value2: %s", data.value2.c_str());
+}
+
+std::function<void(TestObjectiveData&)> getEditorFunction() { return renderTestObjectiveEditor; }
+
+Objective makeTestObjective() {
+	return Objective(TEST_OBJECTIVE_TYPE, _("objectives." + std::string(TEST_OBJECTIVE_TYPE) + ".name"),
+	                 TestObjectiveData{}, getEditorFunction());
+}
+
+ObjectiveBuilderData createTestObjectiveBuilder() {
+	return ObjectiveBuilderData{.type = TEST_OBJECTIVE_TYPE,
+	                            .category = "test.test1.test2",
+	                            .builder = makeTestObjective};
+}
