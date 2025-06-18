c399ce991fbaed1904ed529473dcb78c1f7a75b2
GeTechG <sergey2003.25.9@gmail.com>
2025-06-18 23:54:33 +0500

    feat(lua_define_type.h): increase max members count in SOL_LUA_FOR_EACH to 20

--- LDYOM2/src/utils/lua_define_type.h
+++ LDYOM2/src/utils/lua_define_type.h
@@ -7,11 +7,13 @@
 // Вспомогательные макросы для работы с переменным числом аргументов
 
 // 1. SOL_LUA_PP_ARG_N остается таким же: извлекает N-й аргумент после списка заполнителей.
-#define SOL_LUA_PP_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, N, ...) N
+#define SOL_LUA_PP_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, N, \
+                         ...)                                                                                          \
+	N
 
 // 2. SOL_LUA_PP_RSEQ_N становится объектно-подобным макросом (без скобок).
 //    Это последовательность чисел, используемая для выбора правильного N.
-#define SOL_LUA_PP_RSEQ_N 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+#define SOL_LUA_PP_RSEQ_N 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
 
 // 3. SOL_LUA_PP_NARG_IMPL - это внутренняя реализация, которая вызывает SOL_LUA_PP_ARG_N.
 //    SOL_LUA_EXPAND используется для обеспечения правильного раскрытия.
@@ -73,7 +75,18 @@
 #define SOL_LUA_FOR_EACH_16(what, usertype, ClassType, x, ...)                                                         \
 	what(usertype, ClassType, x);                                                                                      \
 	SOL_LUA_EXPAND(SOL_LUA_FOR_EACH_15(what, usertype, ClassType, __VA_ARGS__))
-// При необходимости можно добавить больше SOL_LUA_FOR_EACH_N макросов для поддержки большего количества членов.
+#define SOL_LUA_FOR_EACH_17(what, usertype, ClassType, x, ...)                                                         \
+	what(usertype, ClassType, x);                                                                                      \
+	SOL_LUA_EXPAND(SOL_LUA_FOR_EACH_16(what, usertype, ClassType, __VA_ARGS__))
+#define SOL_LUA_FOR_EACH_18(what, usertype, ClassType, x, ...)                                                         \
+	what(usertype, ClassType, x);                                                                                      \
+	SOL_LUA_EXPAND(SOL_LUA_FOR_EACH_17(what, usertype, ClassType, __VA_ARGS__))
+#define SOL_LUA_FOR_EACH_19(what, usertype, ClassType, x, ...)                                                         \
+	what(usertype, ClassType, x);                                                                                      \
+	SOL_LUA_EXPAND(SOL_LUA_FOR_EACH_18(what, usertype, ClassType, __VA_ARGS__))
+#define SOL_LUA_FOR_EACH_20(what, usertype, ClassType, x, ...)                                                         \
+	what(usertype, ClassType, x);                                                                                      \
+	SOL_LUA_EXPAND(SOL_LUA_FOR_EACH_19(what, usertype, ClassType, __VA_ARGS__))
 
 #define SOL_LUA_FOR_EACH(what, usertype, ClassType, ...)                                                               \
 	SOL_LUA_EXPAND(                                                                                                    \
