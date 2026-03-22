4a70261c399a155c95da4d022642b5522993a596
GeTechG <sergey2003.25.9@gmail.com>
2026-03-22 13:21:31 +0500

    feat(logic): add a new boolean negate node and its localization

---
+++ ingame/addons/core/nodes/logic/core.bool.negate.lua
@@ -0,0 +1,34 @@
+local fa = require("fa");
+
+local TYPE = "core.bool.negate"
+
+local desc = {
+    type = TYPE,
+    category = "logic",
+    style = "function",
+    is_pure = true,
+    pins = {
+        {
+            title = loc.trv("value"),
+            type = "bool",
+            dir = "in",
+            default = false,
+        },
+        {
+            title = loc.trv("result"),
+            type = "bool",
+            dir = "out"
+        }
+    },
+    on_draw = function(handle)
+        ImGui.Text(fa.ICON_FA_TILDE)
+    end,
+    on_execute = function(handle)
+        local v = handle:getInput(1)
+        if v == nil then v = handle:getInputDefault(1) or false end
+        handle:setOutput(2, not v)
+        return 0
+    end
+}
+
+node_editor.register(desc)
