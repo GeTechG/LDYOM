1910fd8d72d4993b3fe27ec648bbc8f6db2463a9
GeTechG <sergey2003.25.9@gmail.com>
2026-03-25 02:39:04 +0500

    feat: add Beta 0.9.0 release and update installation documentation

---
+++ src/pages/download/versions/beta0_9_0.tsx
@@ -0,0 +1,19 @@
+import Link from "@docusaurus/Link";
+import styles from "@site/src/pages/download/index.module.css";
+import VerInfoLDYOM from "@site/src/pages/download/verInfoLDYOM";
+
+function Beta0_9_0() {
+    return (<VerInfoLDYOM version="Beta 0.9.0" date=""
+                         changes={
+                            <ul>
+                            </ul>
+                        }
+                         supportOld={false}
+                         buttons={[
+                            <Link to="https://github.com/GeTechG/LDYOM/releases"
+                                  className={styles.button}>Download</Link>
+                        ]}
+                         blogLink=""/>);
+}
+
+export default Beta0_9_0;
