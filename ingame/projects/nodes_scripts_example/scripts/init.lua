66418004e8bbc34c800ba6ffaf8f6501e11eec15
GeTechG <sergey2003.25.9@gmail.com>
2026-03-24 21:21:56 +0500

    feat: improve example code

--- ingame/projects/nodes_scripts_example/scripts/init.lua
+++ ingame/projects/nodes_scripts_example/scripts/init.lua
@@ -1,51 +1,35 @@
 local sweet_handler = require("sweet_handler")
 
----@type fun() | nil
-local on_objective_started_disconnect = nil
+local SCENE_ID = "1774287674"
 
----@type fun() | nil
-local on_scene_started_disconnect = nil
-
----@type fun() | nil
-local on_scene_ended_disconnect = nil
-
----@type fun() | nil
-local on_project_stopped_disconnect = nil
+local disconnectors = {}
 
 register_project_script({
     on_load = function()
-        on_objective_started_disconnect = require("on_objective_handler")
-        on_scene_started_disconnect = events.on_scene_started(function(sceneId)
-            if project_player.current_scene_id == "1774287674" then
-                TextOp.addLabel("SWINT", "Press 'E' to interact");
-                TextOp.addLabel("TESMENU", "Choice");
-                TextOp.addLabel("TESTM1", "What happened?");
-                TextOp.addLabel("TESTM2", "Not now.");
-                TextOp.addLabel("PACKNOT", "Sweet: Did you bring the package?");
+        disconnectors[1] = require("on_objective_handler")
+        disconnectors[2] = events.on_scene_started(function(sceneId)
+            if project_player.current_scene_id == SCENE_ID then
+                TextOp.addLabel("SWINT", "Press 'E' to interact")
+                TextOp.addLabel("TESMENU", "Choice")
+                TextOp.addLabel("TESTM1", "What happened?")
+                TextOp.addLabel("TESTM2", "Not now.")
+                TextOp.addLabel("PACKNOT", "Sweet: Did you bring the package?")
             end
         end)
-        on_scene_ended_disconnect = events.on_scene_ended(function(sceneId)
+        disconnectors[3] = events.on_scene_ended(function(sceneId)
             if project_player.is_playing then
-                sweet_handler.on_unload();
+                sweet_handler.on_unload()
             end
         end)
-        on_project_stopped_disconnect = events.on_project_stopped(function()
-            sweet_handler.on_unload();
+        disconnectors[4] = events.on_project_stopped(function()
+            sweet_handler.on_unload()
         end)
     end,
     on_unload = function()
-        if on_objective_started_disconnect then
-            on_objective_started_disconnect()
+        for _, disconnect in ipairs(disconnectors) do
+            disconnect()
         end
-        if on_scene_started_disconnect then
-            on_scene_started_disconnect()
-        end
-        if on_scene_ended_disconnect then
-            on_scene_ended_disconnect()
-        end
-        if on_project_stopped_disconnect then
-            on_project_stopped_disconnect()
-        end
-        sweet_handler.on_unload();
+        disconnectors = {}
+        sweet_handler.on_unload()
     end,
 })
--- ingame/projects/nodes_scripts_example/scripts/sweet_handler.lua
+++ ingame/projects/nodes_scripts_example/scripts/sweet_handler.lua
@@ -1,71 +1,73 @@
 local sweet_handler = {}
 
-local sweet_actor_id = "cf3cc73e-610b-4707-910d-20bc2f2d8144";
+local SWEET_ACTOR_ID = "cf3cc73e-610b-4707-910d-20bc2f2d8144"
+
+local function get_sweet_ped()
+    return ActorComponent.cast(scenes.find_entity(SWEET_ACTOR_ID):getComponent("actor")):getPedRef()
+end
 
 sweet_handler.on_interact = function()
-    tasks.cancel("on_interact_sweet");
+    tasks.cancel("on_interact_sweet")
     tasks.run("on_interact_sweet", function()
-        global_vars.set("interact_sweet", false);
-        local sweet = ActorComponent.cast(scenes.find_entity(sweet_actor_id):getComponent("actor")):getPedRef()
+        global_vars.set("interact_sweet", false)
+        local sweet = get_sweet_ped()
         while not global_vars.get("interact_sweet") do
-            local x, y, z = CharOp.getCoordinates(sweet);
+            local x, y, z = CharOp.getCoordinates(sweet)
             if CharOp.locateOnFoot2D(PlayerOp.getChar(0), x, y, 1.0, 1.0, false) then
-                TextOp.print("SWINT", 100, 0);
-                if (PadOp.isKeyJustPressed(69)) then
-                    global_vars.set("interact_sweet", true);
-                    if project_player.current_objective_index == 1 then
-                        project_player:send_signal("default");
-                    elseif project_player.current_objective_index == 5 then
-                        sweet_handler.not_collect_say();
-                    elseif project_player.current_objective_index == 6 then
-                        project_player:send_signal("default");
+                TextOp.print("SWINT", 100, 0)
+                if PadOp.isKeyJustPressed(69) then
+                    global_vars.set("interact_sweet", true)
+                    local obj_index = project_player.current_objective_index
+                    if obj_index == 1 or obj_index == 6 then
+                        project_player:send_signal("default")
+                    elseif obj_index == 5 then
+                        sweet_handler.not_collect_say()
                     end
                 end
             end
-            tasks.yield();
+            tasks.yield()
         end
     end)
 end
 
 sweet_handler.say = function()
-    local sweet = ActorComponent.cast(scenes.find_entity(sweet_actor_id):getComponent("actor")):getPedRef()
-    TaskOp.playAnim(sweet, "IDLE_chat", "PED", 1, true, false, false, false, 3000)
+    TaskOp.playAnim(get_sweet_ped(), "IDLE_chat", "PED", 1, true, false, false, false, 3000)
 end
 
 sweet_handler.choice = function()
-    tasks.cancel("sweet_choice");
+    tasks.cancel("sweet_choice")
     tasks.run("sweet_choice", function()
-        local menu = MenuOp.create("TESMENU", 240, 180, 180, 1, true, false, 0);
+        local menu = MenuOp.create("TESMENU", 240, 180, 180, 1, true, false, 0)
         MenuOp.setColumn(menu, 0, "DUMMY", "TESTM1", "TESTM2", "DUMMY", "DUMMY", "DUMMY", "DUMMY", "DUMMY", "DUMMY",
-            "DUMMY", "DUMMY", "DUMMY", "DUMMY");
-        while not (global_vars.get("selected_choice") >= 0) do
-            global_vars.set("selected_choice", MenuOp.getItemAccepted(menu));
-            tasks.yield();
+            "DUMMY", "DUMMY", "DUMMY", "DUMMY")
+        local choice
+        repeat
+            choice = MenuOp.getItemAccepted(menu)
+            tasks.yield()
+        until choice >= 0
+        MenuOp.delete(menu)
+        if choice == 0 then
+            project_player:send_signal("help")
+        elseif choice == 1 then
+            project_player:send_signal("exit")
+            PlayerOp.setControl(0, true)
         end
-        MenuOp.delete(menu);
-        if global_vars.get("selected_choice") == 0 then
-            project_player:send_signal("help");
-        elseif global_vars.get("selected_choice") == 1 then
-            project_player:send_signal("exit");
-            PlayerOp.setControl(0, true);
-        end
-        global_vars.set("selected_choice", -1);
     end)
 end
 
 sweet_handler.not_collect_say = function()
     tasks.run("not_collect_say", function()
-        sweet_handler.say();
-        TextOp.printNow("PACKNOT", 2000, 0);
-        tasks.sleep(2000);
-        sweet_handler.on_interact();
+        sweet_handler.say()
+        TextOp.printNow("PACKNOT", 2000, 0)
+        tasks.sleep(2000)
+        sweet_handler.on_interact()
     end)
 end
 
 sweet_handler.on_unload = function()
-    tasks.cancel("on_interact_sweet");
-    tasks.cancel("sweet_choice");
-    tasks.cancel("not_collect_say");
+    tasks.cancel("on_interact_sweet")
+    tasks.cancel("sweet_choice")
+    tasks.cancel("not_collect_say")
 end
 
 return sweet_handler
